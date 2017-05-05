5f5dfce16c67c3bd0252b653d34cd14a5ba6ba50
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-05 07:51:39 -0500

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,19 @@
+public class Employee{
+
+private String username;
+private String password;
+
+  public Employee(String user, String pass){
+    username = user;
+    password = pass;
+  }
+  
+  public String getUsername(){
+    return username;
+  }
+   
+  public String getPassword(){
+    return password;
+  }
+
+}
