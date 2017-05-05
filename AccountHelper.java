ae1247924fb8381ebe38fcaa4e8ee514f24d82ed
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-05 07:48:55 -0500

    Update AccountHelper.java

--- AccountHelper.java
+++ AccountHelper.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+
 public class AccountHelper{
+  private HashMap<String, String> admins;
+  private HashMap<String, String> workers;
+  
+  public AccountHelper(String username, String password){
+    admins.put(username, password);
+  }
+  
 }
