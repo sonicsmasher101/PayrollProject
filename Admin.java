900821fa461d2aa780bbf323a5a46f77a134c593
LIL WINDBREAKER <yogamah1349@k12.ipsd.org>
2017-05-05 08:11:13 -0500

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,5 +1,13 @@
+/**
+	Class used to represent an Admin, which is an Employee
+*/
 public class Admin extends Employee
 {
+	/**
+		Consructs an Admin
+		@param user username
+		@param pass password
+	*/
 	public Admin(String user, String pass)
 	{
 		super(user, pass);
