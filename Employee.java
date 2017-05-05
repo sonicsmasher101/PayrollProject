36d6545725c0efcadbefd4d992b4fca60eb1f79b
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-05 08:13:09 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -2,18 +2,54 @@ public class Employee{
 
 private String username;
 private String password;
-
+private int clockInTime;
+private int clockOutTime;
+  
+  /**
+  *Creates an Employee with username and password
+  *@param Employee's username
+  *@param Employee's password
+  */
   public Employee(String user, String pass){
     username = user;
     password = pass;
   }
   
+  /**
+  *Gives employee's username
+  *@return Username
+  */
   public String getUsername(){
     return username;
   }
    
+  /**
+  *Gives employee's password
+  *@return Password
+  */
   public String getPassword(){
     return password;
   }
+  
+  /**
+  *Clocks in Employee and refreshes clock out time
+  */
+  public void clockIn(){
+    clockInTime = System.timeInMillis();
+    clockOutTime = System.timeInMillis();
+  }
+  
+  /**
+  *Clocks out Employee
+  */
+  public void clockOut()[
+    clockOutTime = System.timeInMillis();
+  }
+    
+  /**
+  *Gives last clock in time
+  *@return Clock in time
+  */
+  public 
 
 }
