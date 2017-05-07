8f3e359e493db06c83713aa0b8d363ed8dd6728f
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-06 21:14:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import javax.swing.JPanel;
+
 public class Main{
 
   public static void main(String[] args){
+    JPanel panel = new JPanel("Payroll Application");
+    panel.setDefaultCloseOperation(JPanel.EXIT_ON_CLOSE);
+    panel.pack();
+    panel.setVisible(true);
   }
   
 }
