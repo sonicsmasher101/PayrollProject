f100209df8701960d9a99ba1699d81011d15f951
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-06 20:04:01 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Tester{
   
   public static void main(String[] args){
-    System.out.println("Give us a 100%");
+    Scanner in = new Scanner(System.in);
+    Employee employee = new Employee("Rishi Menon", 1086);
+    employee.clockIn();
+    System.out.println("Press enter when you want to clock out employee!");
+    in.next();
+    employee.clockOut();
+    System.out.println("Employee wages: " employee.getWages());
   }
 }
