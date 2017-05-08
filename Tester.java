c4153176e99c850b99a720f26f5709e65b035db8
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-08 07:30:22 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -4,11 +4,16 @@ public class Tester{
   
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
+    
+    //Testing employee class
     Employee employee = new Employee("Rishi Menon", 1086);
     employee.clockIn();
     System.out.println("Press enter when you want to clock out employee!");
     in.next();
     employee.clockOut();
     System.out.println("Employee wages: " employee.getPay());
+    
+    Admin admin = new Admin("administrator", "password");
+    
   }
 }
