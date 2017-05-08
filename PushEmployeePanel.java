58a1358145ff616e5dfc21d31c5c69145cf4c7d0
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-08 16:41:34 -0500

    Create PushEmployeePanel.java

---
+++ PushEmployeePanel.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class PushEmployeePanel extends JPanel{
+	//Class to be implemented fully
+	public PushEmployeePanel(){
+		
+		setPreferredSize(new Dimension(300, 400));
+		setBackground(Color.black);
+	}
+	
+	private class ButtonListener implements ActionListener{
+		
+		public void actionPerformed(ActionEvent event){
+			
+		}
+		
+	}
+}
