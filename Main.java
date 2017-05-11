fe4b671fdbe46e54d6d3977fdfb584bf7c68956e
gardon712 <jonathancordero@me.com>
2017-05-11 07:29:37 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
 import javax.swing.JFrame;
 
 public class Main{
-
-  public static void main(String[] args){
+    public static void main(String[] args){
     JFrame frame = new JFrame("Payroll Application");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 400);
     frame.setLocationRelativeTo(null);
     frame.add(new PushCounterPanel());
     frame.setVisible(true);
+    //Gui code goes here
+      
+      while(true)
+    {
+      
+    }
   }
 }
