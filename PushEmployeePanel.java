0349f9723894f5c3c7c24b0ce6ec63cae9fc7462
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-11 18:19:20 -0500

    Update PushEmployeePanel.java

--- PushEmployeePanel.java
+++ PushEmployeePanel.java
@@ -1,34 +1,42 @@
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
-import java.lang.IllegalArgumentException;
+import javax.swing.*;;
 
 public class PushEmployeePanel extends JPanel{
 private AccountHelper helper;
+private JButton button;
 	public PushEmployeePanel(AccountHelper helper){
 		this.helper = helper;
 		setPreferredSize(new Dimension(300, 400));
-		setBackground(Color.black);
+		setBackground(Color.DARK_GRAY);
+		button = new JButton("Employee Login");
+		button.addActionListener(new ButtonListener());
+		add(button);
 	}
 	
 	private class ButtonListener implements ActionListener{
 		//Needs to be properly completed, error code may be incorrect
 		public void actionPerformed(ActionEvent event){
-			JOptionPane pane = new JOptionPane();
 			String username = "";
-			while(!helper.checkUsername(username)) username = pane.showInputDialog("Input correct username:");
-			String id;
-			int idNum = AccountHelper.getID(username);
+			boolean cont = true;
+			while(!helper.checkUsername(username) && cont){
+				username = JOptionPane.showInputDialog("Input correct username:");
+				if(username.equals("")) cont = false;
+			}
+
+			String id = "-1";
+			int idNum = helper.getID(username);
 			while(helper.getID(username) != Integer.parseInt(id)){
-				id = pane.showInputDialog("Enter id:");
+				id = JOptionPane.showInputDialog("Enter id:");
 				try{
 				Integer.parseInt(id);
 				}
-				catch(IllegalArgumentException e){
+				catch(NumberFormatException e){
 				System.err.println("Integer not entered!");
 				}
-				pane.showOutputDialog("DERP!");
 			}
+			JOptionPane.showMessageDialog(null, "DERP!");
+			
 		}
 		
 	}
