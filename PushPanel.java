51f63e578b98448ec2c9b3fe01cf613655bb4193
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-12 08:01:12 -0500

    Update PushPanel.java

--- PushPanel.java
+++ PushPanel.java
@@ -6,16 +6,20 @@ public class PushPanel extends JPanel{
 private AccountHelper helper;
 private JButton eButton;
 private JButton aButton;
+private JLabel succ;
 	public PushPanel(AccountHelper helper){
 		this.helper = helper;
 		setPreferredSize(new Dimension(300, 400));
 		setBackground(Color.DARK_GRAY);
 		eButton = new JButton("Employee Login");
 		aButton = new JButton("Admin Login");
-		eBtton.addActionListener(new EmployeeButtonListener());
+		eButton.addActionListener(new EmployeeButtonListener());
 		aButton.addActionListener(new AdminButtonListener());
 		add(eButton);
 		add(aButton);
+		succ.setIcon(new ImageIcon("SUCC.jpg"));
+		succ.setText("SUCC");
+		add(succ);
 	}
 	
 	private class EmployeeButtonListener implements ActionListener{
@@ -42,23 +46,27 @@ private JButton aButton;
 			JOptionPane.showMessageDialog(null, "DERP!");
 			
 		}
+	}
 		
 	private class AdminButtonListener implements ActionListener{
 		//Needs to be properly completed, error code may be incorrect
 		public void actionPerformed(ActionEvent event){
 			String username = "";
 			boolean cont = true;
-			while(!helper.checkUsername(username) && cont){
+			while(!helper.getAdminUsername().equals(username) && cont){
 				username = JOptionPane.showInputDialog("Input username:");
 				if(username.equals("")) cont = false;
 			}
 
 			String pass = "";
 			String password = helper.getAdminPassword();
-			while(!pass.equals(password)){
-				id = JOptionPane.showInputDialog("Enter password:");
+			int count = 3;
+			while(!pass.equals(password) && count > 0){
+				pass = JOptionPane.showInputDialog("Enter password:");
+				count--;
 			}
-			JOptionPane.showMessageDialog(null, "DERP!");
+			if(count != 0) JOptionPane.showMessageDialog(null, "DERP!");
+			else JOptionPane.showMessageDialog(null, "Incorrect Password Entered Too Many Times", "Incorrect Password Entered Too Many Times", JOptionPane.ERROR_MESSAGE);
 			
 		}
 		
