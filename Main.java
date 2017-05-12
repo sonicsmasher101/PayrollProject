41a83327be1e157abd347498f7ed94144b746844
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-12 07:28:27 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main{
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 400);
     frame.setLocationRelativeTo(null);
-    public static AccountHelper Tarun = new AccountHelper("Tarun", "AD");
-    Tarun.addEmployee(new Employee("Jonathan Corderdo", 6969));
+    AccountHelper Tarun = new AccountHelper("Tarun", "AD");
+    Tarun.addEmployee(new Employee("Jonathan Corderdo", 6969, 420.69));
     frame.add(new PushEmployeePanel(Tarun));
     frame.setVisible(true);
     //Gui code goes here
