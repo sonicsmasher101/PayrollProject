d83001adeafd4dd8e9374f6fe93c280734957307
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-17 08:02:47 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class Tester{
-  
-  public static void main(String[] args){
+  public static JFrame adminFrame;
     public static void main(String[] args){
     JFrame frame = new JFrame("Payroll Application");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 400);
     frame.setLocationRelativeTo(null);
     AccountHelper Tarun = new AccountHelper("Tarun", "AD");
+    adminFrame = new JFrame("Admin");
+    adminFrame.add(new AdminPanel(Tarun));
+    adminFrame.setSize(400, 400);
+    adminFrame.setVisible(false);
     Tarun.addEmployee(new Employee("Jonathan Cordero", 6969, 420.69));
     frame.add(new PushPanel(Tarun));
     frame.setVisible(true);
