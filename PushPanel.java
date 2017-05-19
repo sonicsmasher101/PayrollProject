819e8782488dd0184a5c599f0b7cd9656191a313
LIL WINDBREAKER <yogamah1349@k12.ipsd.org>
2017-05-18 22:40:31 -0500

    Update PushPanel.java

--- PushPanel.java
+++ PushPanel.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payroll;
+
+/**
+ *
+ * @author Yoga Mahartayasa
+ */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;;
@@ -7,21 +18,25 @@ private AccountHelper helper;
 private JButton eButton;
 private JButton aButton;
 private JButton eBButton;
+private JButton aBButton;
 private JLabel succ;
 	public PushPanel(AccountHelper helper){
 		this.helper = helper;
 		setPreferredSize(new Dimension(300, 400));
 		setBackground(Color.DARK_GRAY);
 		eButton = new JButton("Employee Login");
-		aButton = new JButton("Admin Login");
                 eBButton = new JButton("Employee Logout");
+                aButton = new JButton("Admin Login");
+                aBButton = new JButton("Admin Logout");
 		succ = new JLabel();
 		eButton.addActionListener(new EmployeeButtonListener());
 		aButton.addActionListener(new AdminButtonListener());
                 eBButton.addActionListener(new EmployeeButtonListenerB());
+                aBButton.addActionListener(new AdminButtonListenerB());
 		add(eButton);
 		add(aButton);
                 add(eBButton);
+                add(aBButton);
 		succ.setIcon(new ImageIcon("src\\SUCC.jpg"));
 		succ.setText("SUCC");
 		add(succ);
@@ -80,8 +95,7 @@ private JLabel succ;
 			}
                         t =helper.getEmployee(username);
                         t.logout();
-                        double pay = t.getPay();
-			JOptionPane.showMessageDialog(null, "Logout Successful, pay: " + pay);
+			JOptionPane.showMessageDialog(null, "Logout Successful");
                         
 			
 		}
@@ -105,7 +119,36 @@ private JLabel succ;
 				count--;
 			}
 			if(count != 0){
-                            JOptionPane.showMessageDialog(null, "DERP!");
+                            JOptionPane.showMessageDialog(null, "Login successful");
+                            helper.getAdmin().login();
+                            
+                        }
+			else JOptionPane.showMessageDialog(null, "Incorrect Password Entered Too Many Times", "Incorrect Password Entered Too Many Times", JOptionPane.ERROR_MESSAGE);
+			
+		}
+		
+	}
+        
+        private class AdminButtonListenerB implements ActionListener{
+		//Needs to be properly completed, error code may be incorrect
+		public void actionPerformed(ActionEvent event){
+			String username = "";
+			boolean cont = true;
+			while(!helper.getAdminUsername().equals(username) && cont){
+				username = JOptionPane.showInputDialog("Input username:");
+				if(username.equals("")) cont = false;
+			}
+
+			String pass = "";
+			String password = helper.getAdminPassword();
+			int count = 3;
+			while(!pass.equals(password) && count > 0){
+				pass = JOptionPane.showInputDialog("Enter password:");
+				count--;
+			}
+			if(count != 0){
+                            JOptionPane.showMessageDialog(null, "Logout successful");
+                            helper.getAdmin().logout();
                             
                         }
 			else JOptionPane.showMessageDialog(null, "Incorrect Password Entered Too Many Times", "Incorrect Password Entered Too Many Times", JOptionPane.ERROR_MESSAGE);
