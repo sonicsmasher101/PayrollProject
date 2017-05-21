9cbe7ac89c9b5739f2a4323286302f23549188f3
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-21 12:29:14 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -7,11 +7,16 @@ public class Tester{
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 400);
     frame.setLocationRelativeTo(null);
+    ImageIcon icon = new ImageIcon("src\\SUCC.jpg");
+    frame.setIconImage(icon.getImage());
+    frame.setResizable(false);
     AccountHelper Tarun = new AccountHelper("Tarun", "AD");
     adminFrame = new JFrame("Admin");
     adminFrame.add(new AdminPanel(Tarun));
     adminFrame.setSize(400, 400);
+    adminFrame.setLocationRelativeTo(null);
     adminFrame.setVisible(false);
+    adminFrame.setResizable(false);
     Tarun.addEmployee(new Employee("Jonathan Cordero", 6969, 420.69));
     frame.add(new PushPanel(Tarun));
     frame.setVisible(true);
