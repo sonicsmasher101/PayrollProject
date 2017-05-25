a89dc940ee3021a401046df6ff75bcd515c50386
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-25 07:23:29 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
 import javax.swing.*;
-import java.io.FileNotFoundException;
+import java.io.*;
 
 public class Main{
   public static JFrame adminFrame;
     public static void main(String[] args) throws FileNotFoundException{
+    File input = new File("input.txt");
+    Scanner in = new Scanner(input);
     JFrame frame = new JFrame("Payroll Application");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 350);
@@ -19,7 +21,10 @@ public class Main{
     adminFrame.setLocationRelativeTo(null);
     adminFrame.setVisible(false);
     adminFrame.setResizable(false);
-    Tarun.addEmployee(new Employee("Jonathan Cordero", 6969, 420.69));
+    while(in.hasNext()){
+      String[] inputs = in.next().split(",");
+      Tarun.add(new Employee(inputs[0], Integer.parseInt(inputs[1]), Double.parseDouble(inputs[2]));
+    }
     frame.add(new PushPanel(Tarun));
     frame.setVisible(true);
   }
