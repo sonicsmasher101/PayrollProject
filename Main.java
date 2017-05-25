82e1348083e84d90cf3a9f0b5b40749e2b9e4be5
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-25 17:26:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,50 @@
-import java.util.*;
+import java.util.*;  
 import javax.swing.*;
+import java.awt.*;
 import java.io.*;
+import java.awt.event.*;
 
 public class Main{
   public static JFrame adminFrame;
     public static void main(String[] args) throws FileNotFoundException{
     JFrame frame = new JFrame("Payroll Application");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(400, 350);
+    frame.setSize(400, 400);
     frame.setLocationRelativeTo(null);
-    ImageIcon icon = new ImageIcon("SUCC.gif");
+    ImageIcon icon = new ImageIcon("src\\SUCC.jpg");
     frame.setIconImage(icon.getImage());
     frame.setResizable(false);
-    AccountHelper Tarun = new AccountHelper("Tarun", "AD");
+    AccountHelper Tarun = new AccountHelper("Tarun", "AD", true);
+    Scanner in = new Scanner(new File("input.txt"));
+    	String temporary = null;
+    	if(in.hasNextLine()) temporary = in.nextLine().substring(4);
+    	if(!temporary.equals("")){
+    	String[] sections = temporary.split(" ");
+    	for(String element : sections){
+    		String[] temp = element.split(",");
+    		Tarun.addEmployee(new Employee(temp[0], Integer.parseInt(temp[1]), Double.parseDouble(temp[2]), Long.parseLong(temp[3])));
+    	}
+    	}
+    in.close();
     adminFrame = new JFrame("Admin");
     adminFrame.add(new AdminPanel(Tarun));
-    adminFrame.setSize(400, 500);
+    adminFrame.setSize(400, 400);
     adminFrame.setLocationRelativeTo(null);
     adminFrame.setVisible(false);
     adminFrame.setResizable(false);
     frame.add(new PushPanel(Tarun));
+    PrintWriter writer = new PrintWriter("input.txt");
+    frame.addWindowListener(new WindowAdapter() {
+ 	   public void windowClosing(WindowEvent event) {
+ 		 String list = null;
+ 	     for(Employee element : Tarun.getEmployees()){
+ 	    	 list += element.toString() + " ";
+ 	     }
+ 	     writer.println(list);
+ 	     writer.close();
+ 	     frame.setVisible(false);
+ 	   }
+ 	  });
     frame.setVisible(true);
   }
 }
