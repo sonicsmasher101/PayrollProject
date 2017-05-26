814d64c0c3cd3c48d0d17ee74ee252dfb64cdb31
sonicsmasher101 <menon.tarun@gmail.com>
2017-05-25 19:36:26 -0500

    Update AccountHelper.java

--- AccountHelper.java
+++ AccountHelper.java
@@ -11,22 +11,22 @@ public class AccountHelper{
   *@param Username for first admin
   *@param Password for first admin
   */
-  public AccountHelper(String username, String password, boolean write){
-    admin = new Admin(username, password, write);
+  public AccountHelper(String username, String password){
+    admin = new Admin(username, password);
     employeePasswords = new HashMap<String, Integer>();
     employees = new ArrayList<Employee>();
   }
   
   /**
    * Gives whole arraylist of employees
-   * @return employees
+   * @return employes
    */
   public ArrayList<Employee> getEmployees(){
 	  return employees;
   }
   
    /**
-  *Adds new employee to system
+  *Adds new admin to system
   *@param Employee object of new employee
   */
   public void addEmployee(Employee e){
@@ -60,7 +60,7 @@ public class AccountHelper{
   
   /**
   *Gives password of the admin
-  *@return The admin password
+  *@return The admin username
   */
   public String getAdminPassword(){
     return admin.getPassword();
@@ -68,7 +68,7 @@ public class AccountHelper{
   
   /**
    *Gives username of the admin
-   * @return The admin username
+   * @return The admin password
    */
   public String getAdminUsername(){
 	return admin.getUsername();
@@ -76,7 +76,7 @@ public class AccountHelper{
   
   /**
   *Gives password of an employee given their username if the username exists, will return null otherwise
-  *@param The employee id
+  *@param The employee username
   */
   public int getID(String user){
     if(checkUsername(user)) return (Integer) employeePasswords.get(user);
@@ -103,8 +103,8 @@ public class AccountHelper{
 }
   
   /**
-  *Gives employee based on their name
-  *@return employee
+  *Gives index of employee searched for, or -1 if they don't exist
+  *@return index
   */
   public Employee getEmployee(String name)
   {
